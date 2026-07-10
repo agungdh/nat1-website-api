cbd696c2ea6f69ee75cdd6de6e2dbfa590670bca
AgungDH <agungdh@live.com>
2026-07-10 11:45:04 +0700

    init

---
+++ src/main/java/id/my/agungdh/nat1_website_api/Nat1WebsiteApiApplication.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.nat1_website_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nat1WebsiteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nat1WebsiteApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/agungdh/nat1_website_api/Nat1WebsiteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.nat1_website_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nat1WebsiteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
