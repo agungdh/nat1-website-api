ce9ce959d267a5354ea38b69579a1102196603e1
AgungDH <agungdh@live.com>
2026-07-10 14:38:52 +0700

    configure scheduler with virtual threads (SimpleAsyncTaskScheduler)

--- src/main/java/id/my/agungdh/nat1_website_api/config/SchedulerConfig.java
+++ src/main/java/id/my/agungdh/nat1_website_api/config/SchedulerConfig.java
@@ -1,9 +1,21 @@
 package id.my.agungdh.nat1_website_api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.SimpleAsyncTaskScheduler;
+
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableScheduling
 public class SchedulerConfig {
+
+    @Bean
+    public SimpleAsyncTaskScheduler taskScheduler() {
+        SimpleAsyncTaskScheduler scheduler = new SimpleAsyncTaskScheduler();
+        scheduler.setVirtualThreads(true);
+        scheduler.setThreadNamePrefix("scheduler-");
+        return scheduler;
+    }
 }
